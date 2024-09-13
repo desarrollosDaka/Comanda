794beb14b343b9166062f61497dee3b4ccd3d72f
ARSYSTEMAS <andersonrafael.garciagil@gmail.com>
2024-09-13 14:10:54 -0400

    se agrego el recordar contrasena en login

--- frontend/src/views/authentication/authForms/AuthLogin.vue
+++ frontend/src/views/authentication/authForms/AuthLogin.vue
@@ -1,94 +1,97 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 // icons
 import { EyeInvisibleOutlined, EyeOutlined } from '@ant-design/icons-vue';
 import { useAuthStore } from '@/stores/auth';
 import { Form } from 'vee-validate';
 import Logo from '@/layouts/dashboard/logo/LogoDark.vue';
+import { useLocalStorage } from "@vueuse/core";
 
-const checkbox = ref(false);
-const valid = ref(false);
-const show1 = ref(false);
-const Password = ref('');
-const Email = ref('');
-const errors = ref('contraseña incorrecta');
+const remenberMe = ref<boolean>(false);
+const valid = ref<boolean>(false);
+const show1 = ref<boolean>(false);
+const Password = ref<string>('');
+const Email = ref<string>('');
+const errors = ref<string>('contraseña incorrecta');
+
+const storedEmail = useLocalStorage('Email', '');
+const storedPassword = useLocalStorage('Password', '');
+
+onMounted(() => {
+
+  if (storedEmail.value && storedPassword.value) {
+    Email.value = storedEmail.value;
+    Password.value = storedPassword.value;
+    remenberMe.value = true;
+  }
+})
 const passwordRules = ref([
   (v: string) => !!v || 'La contraseña es requerida',
   (v: string) => (v && v.length <= 10) || 'La contraseña debe tener menos de 10 caracteres.'
 ]);
 
 const emailRules = ref([
-  (v: string) => !!v || 'El email es requerido', 
+  (v: string) => !!v || 'El email es requerido',
   (v: string) => /.+@.+\..+/.test(v) || 'El email debe ser válido'
 ]);
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 function validate(values: any, { setErrors }: any) {
+  useLocalStorageAuthentication()
   const authStore = useAuthStore();
   return authStore.login(Email.value, Password.value).catch((error) => setErrors({ apiError: 'Usuario o contraseña incorrecta' }));
+}
+
+function useLocalStorageAuthentication() {
+
+  if (remenberMe.value) {
+    storedEmail.value = Email.value;
+    storedPassword.value = Password.value;
+  } else {
+    storedEmail.value = '';
+    storedPassword.value = '';
+  }
 }
 </script>
 
 <template>
   <div class="d-flex justify-space-between align-center">
     <div class="logoDakaLogin">
-        <Logo />
-      </div>
+      <Logo />
+    </div>
     <!-- <router-link to="/auth/register" class="text-primary text-decoration-none">Don't Have an account?</router-link> -->
   </div>
   <Form @submit="validate" class="mt-7 loginForm" v-slot="{ errors, isSubmitting }">
     <div class="mb-6">
       <v-label>Correo Electronico</v-label>
-      <v-text-field
-        aria-label="email address"
-        v-model="Email"
-        :rules="emailRules"
-        class="mt-2"
-        required
-        hide-details="auto"
-        variant="outlined"
-        color="primary"
-      ></v-text-field>
+      <v-text-field aria-label="email address" v-model="Email" :rules="emailRules" class="mt-2" required
+        hide-details="auto" variant="outlined" color="primary"></v-text-field>
     </div>
     <div>
       <v-label>Contraseña</v-label>
-      <v-text-field
-        aria-label="password"
-        v-model="Password"
-        :rules="passwordRules"
-        required
-        variant="outlined"
-        color="primary"
-        hide-details="auto"
-        :type="show1 ? 'text' : 'password'"
-        class="pwdInput mt-2"
-      >
+      <v-text-field aria-label="password" v-model="Password" :rules="passwordRules" required variant="outlined"
+        color="primary" hide-details="auto" :type="show1 ? 'text' : 'password'" class="pwdInput mt-2">
         <template v-slot:append>
           <v-btn color="secondary" icon rounded variant="text">
-            <EyeInvisibleOutlined :style="{ color: 'rgb(var(--v-theme-secondary))' }" v-if="show1 == false" @click="show1 = !show1" />
-            <EyeOutlined :style="{ color: 'rgb(var(--v-theme-secondary))' }" v-if="show1 == true" @click="show1 = !show1" />
+            <EyeInvisibleOutlined :style="{ color: 'rgb(var(--v-theme-secondary))' }" v-if="show1 == false"
+              @click="show1 = !show1" />
+            <EyeOutlined :style="{ color: 'rgb(var(--v-theme-secondary))' }" v-if="show1 == true"
+              @click="show1 = !show1" />
           </v-btn>
         </template>
       </v-text-field>
     </div>
 
     <div class="d-flex align-center mt-4 mb-7 mb-sm-0">
-      <v-checkbox
-        v-model="checkbox"
-        :rules="[(v: any) => !!v || 'You must agree to continue!']"
-        label="Recordar"
-        required
-        color="primary"
-        class="ms-n2"
-        hide-details
-      ></v-checkbox>
+      <v-checkbox v-model="remenberMe" :rules="[(v: any) => !!v || 'You must agree to continue!']" label="Recordar"
+        required color="primary" class="ms-n2" hide-details></v-checkbox>
       <!-- <div class="ml-auto">
         <router-link to="/auth/login" class="text-darkText link-hover">Forgot Password?</router-link>
       </div> -->
     </div>
-    <v-btn color="primary" :loading="isSubmitting" block class="mt-5" variant="flat" size="large" :disabled="valid" type="submit">
-      Inicia Sesion</v-btn
-    >
+    <v-btn color="primary" :loading="isSubmitting" block class="mt-5" variant="flat" size="large" :disabled="valid"
+      type="submit">
+      Inicia Sesion</v-btn>
     <div v-if="errors.apiError" class="mt-2">
       <v-alert color="error">{{ errors.apiError }}</v-alert>
     </div>
@@ -96,17 +99,15 @@ function validate(values: any, { setErrors }: any) {
 </template>
 
 <style lang="scss">
-
 .loginForm {
   .v-text-field .v-field--active input {
     font-weight: 500;
   }
 }
 
-.logoDakaLogin{
+.logoDakaLogin {
   margin: 0 auto;
   // margin-left: 0 auto;
-  
+
 }
- 
 </style>
